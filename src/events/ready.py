cb73ab79651e2667f9a7f77b7b17d5b595d2b33a
StarStudio <charlottewiltshire27@outlook.com>
2024-08-02 18:36:18 +0300

    [ADD]: Implement on_ready event handler
    Changes:
    - Added `OnReady` cog to handle `on_ready` event.
    - Logged a message when the bot is online using `loguru`.
    - Registered the `OnReady` cog in the `setup` function.

---
+++ src/events/ready.py
@@ -0,0 +1,16 @@
+from disnake.ext import commands
+from loguru import logger
+
+
+class OnReady(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info(f"{self.bot.user} is now online.")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(OnReady(bot))
+    logger.info(f"Extension {__name__} is ready")
