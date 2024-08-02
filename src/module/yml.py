6a65817df6bb3a5ce7376f5de02e857a8bf357c1
Charlotte <charlottewiltshire27@outlook.com>
2024-08-02 08:01:16 -0400

    clean

--- src/module/yml.py
+++ src/module/yml.py
@@ -5,8 +5,6 @@ class Yml:
     def __init__(self, src):
         self.src = src
 
-    def load(self):
-        with open('info.yaml') as f:
+    def load(self, src):
+        with open(src) as f:
             templates = yaml.safe_load(f)
-
-pprint(templates)
