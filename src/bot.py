17d791251b251a160f9b5a61528823591687b343
StarStudio <charlottewiltshire27@outlook.com>
2024-08-02 18:35:32 +0300

    [ADD]: Improve bot token error handling
    Changes:
    - Imported `LoginFailure` from `disnake`.
    - Integrated `loguru` for logging.
    - Added error handling in `run` function to catch `LoginFailure` and log an error message if the bot token is incorrect.

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,7 @@
 import disnake
+from disnake import LoginFailure
 from disnake.ext import commands
+from loguru import logger
 
 from src.module.utils import loadExtensions
 
@@ -10,5 +12,9 @@ bot = commands.Bot(intents=intents, command_prefix='/')
 loadExtensions(bot, 'src/events')
 loadExtensions(bot, 'src/commands')
 
+
 def run(token):
-    bot.run(token)
+    try:
+        bot.run(token)
+    except LoginFailure:
+        logger.error('Your bot token is incorrect! Shutting down...')
