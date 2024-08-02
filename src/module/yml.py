8bf4dc93e1ad67129cbbad68db6ec4fd47f7f26f
StarStudio <charlottewiltshire27@outlook.com>
2024-08-02 18:09:21 +0300

    [ADD]: Extend Yml class with read and write methods
    Changes:
    - Added `read` method to read YAML data from a file and return it.
    - Added `write` method to write data to a YAML file.
    - Modified `load` method to return the loaded YAML data.

--- src/module/yml.py
+++ src/module/yml.py
@@ -1,10 +1,19 @@
 import yaml
 from pprint import pprint
 
+
 class Yml:
     def __init__(self, src):
         self.src = src
 
     def load(self):
         with open(self.src) as f:
-            templates = yaml.safe_load(f)
+            return yaml.safe_load(f)
+
+    def read(self):
+        with open(self.src, 'r') as f:
+            return yaml.safe_load(f)
+
+    def write(self, data):
+        with open(self.src, 'w') as f:
+            yaml.dump(data, f)
