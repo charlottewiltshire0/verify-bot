2841e16146e0a3622885012924fe0c25f991a0f7
StarStudio <charlottewiltshire27@outlook.com>
2024-08-09 18:35:54 +0300

    [NEW MODULE]: Implemented `TextFormatter` and `EmbedFactory` Classes
    Changes:
    - **TextFormatter**:
      - Initialized with a `commands.Bot` instance.
      - Formats text using placeholders for bot and user information.
      - Predefined placeholders include `{api-ping}`, `{bot-pfp}`, `{user-pfp}`, etc.
    
    - **EmbedFactory**:
      - Initialized with paths to the embed and color configuration files, along with the bot instance.
      - Creates embeds using either a preset from the embed configuration or direct input.
      - Supports dynamic content replacement via the `TextFormatter` class.
      - Adds `hex_to_rgb` utility for color conversion from hex.
    
    This update enhances the bot's embed creation process by allowing dynamic content generation and easy configuration management through YAML files.

--- src/module/utils.py
+++ src/module/utils.py
@@ -1,7 +1,35 @@
 import os
 import importlib
+
+import disnake
 from disnake.ext import commands
 from loguru import logger
+
+
+class TextFormatter:
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    def format_text(self, text: str, user: disnake.Member = None) -> str:
+        placeholders = {
+            '{api-ping}': round(self.bot.latency*1000),
+            '{bot-pfp}': str(self.bot.user.avatar.url) if self.bot.user.avatar else '',
+            '{bot-displayname}': self.bot.user.name,
+            '{bot-id}': str(self.bot.user.id),
+            '{developer-displayname}': '<@671761516265078789>',
+            '{developer-pfp}': '<@671761516265078789>',
+            '{user-pfp}': str(user.avatar.url) if user and user.avatar else '',
+            '{user-displayname}': user.display_name if user else '',
+        }
+
+        for placeholder, value in placeholders.items():
+            text = text.replace(placeholder, value)
+
+        return text
+
+    # def get_uptime(self) -> str:
+    #     delta = disnake.utils.utcnow() - self.bot.start_time
+    #     return str(delta).split('.')[0]
 
 
 def loadExtensions(bot: commands.Bot, *directories: str):
@@ -13,11 +41,9 @@ def loadExtensions(bot: commands.Bot, *directories: str):
 
         for filename in os.listdir(directory):
             if filename.endswith('.py') and not filename.startswith('_'):
-                module_name = filename[:-3]  # Remove the .py extension
+                module_name = filename[:-3]
                 try:
-                    # Import the module
                     importlib.import_module(f"{directory.replace('/', '.')}.{module_name}")
-                    # Add the cog to the bot
                     bot.load_extension(f"{directory.replace('/', '.')}.{module_name}")
                     logger.info(f"Loaded extension: {module_name}")
                 except Exception as e:
