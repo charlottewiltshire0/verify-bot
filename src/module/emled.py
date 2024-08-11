16ed0b1153de61ea21a5cdabb25fbf06be6f02b2
StarStudio <charlottewiltshire27@outlook.com>
2024-08-11 17:14:48 +0300

    [ENHANCED]: Make create_embed method asynchronous
    Changes:
    - Changed `create_embed` method in `emled.py` to be asynchronous.
    - Updated text formatting calls within `create_embed` to use `await` for handling user-specific content.
    - Improved embed field handling by making text formatting asynchronous.

--- src/module/emled.py
+++ src/module/emled.py
@@ -12,42 +12,46 @@ class EmbedFactory:
         self.color_data = Yml(color_config_path).load()
         self.formatter = TextFormatter(bot)
 
-    def create_embed(self, preset: Optional[str] = None, user: disnake.Member = None, **kwargs) -> disnake.Embed:
+    async def create_embed(self, preset: Optional[str] = None, user: disnake.Member = None, **kwargs) -> disnake.Embed:
         embed_dict = self.embed_data.get('Embeds', {}).get(preset, {}) if preset else kwargs
 
+        title = await self.formatter.format_text(embed_dict.get('Title', ''), user=user)
+        description = await self.formatter.format_text(embed_dict.get('Description', ''), user=user)
+        color = disnake.Color.from_rgb(*self.hex_to_rgb(embed_dict.get('Color', self.color_data['EmbedColors'].get('Default', '#242424'))))
+
         embed = disnake.Embed(
-            title=self.formatter.format_text(embed_dict.get('Title', ''), user=user),
-            description=self.formatter.format_text(embed_dict.get('Description', ''), user=user),
+            title=title,
+            description=description,
             url=embed_dict.get('Url', ''),
-            color=disnake.Color.from_rgb(*self.hex_to_rgb(embed_dict.get('Color', self.color_data['EmbedColors'].get('Default', '#242424'))))
+            color=color
         )
 
         if 'Author' in embed_dict:
             embed.set_author(
-                name=self.formatter.format_text(embed_dict['Author'], user=user),
+                name=await self.formatter.format_text(embed_dict['Author'], user=user),
                 url=embed_dict.get('AuthorUrl', None),
-                icon_url=self.formatter.format_text(embed_dict.get('AuthorIcon', None), user=user)
+                icon_url=await self.formatter.format_text(embed_dict.get('AuthorIcon', None), user=user)
             )
 
         if 'Footer' in embed_dict:
             embed.set_footer(
-                text=self.formatter.format_text(embed_dict['Footer'], user=user),
-                icon_url=self.formatter.format_text(embed_dict.get('FooterIcon', None), user=user)
+                text=await self.formatter.format_text(embed_dict['Footer'], user=user),
+                icon_url=await self.formatter.format_text(embed_dict.get('FooterIcon', None), user=user)
             )
 
         if 'Thumbnail' in embed_dict:
-            embed.set_thumbnail(url=self.formatter.format_text(embed_dict['Thumbnail'], user=user))
+            embed.set_thumbnail(url=await self.formatter.format_text(embed_dict['Thumbnail'], user=user))
 
         if 'Image' in embed_dict:
-            embed.set_image(url=self.formatter.format_text(embed_dict['Image'], user=user))
+            embed.set_image(url=await self.formatter.format_text(embed_dict['Image'], user=user))
 
         if embed_dict.get('Timestamp', False):
             embed.timestamp = disnake.utils.utcnow()
 
         for field in embed_dict.get('Fields', []):
             embed.add_field(
-                name=self.formatter.format_text(field.get('name', 'No Name'), user=user),
-                value=self.formatter.format_text(field.get('value', 'No Value'), user=user),
+                name=await self.formatter.format_text(field.get('name', 'No Name'), user=user),
+                value=await self.formatter.format_text(field.get('value', 'No Value'), user=user),
                 inline=field.get('inline', False)
             )
 
@@ -57,7 +61,6 @@ class EmbedFactory:
     def hex_to_rgb(hex_color: str) -> tuple:
         hex_color = hex_color.lstrip('#')
         return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-
 
 # Usage example:
 # embed_factory = EmbedFactory('embeds.yml', 'config.yml')
