0a9e94d21bcd411d04dc2140f10c86023ae1a2e8
StarStudio <charlottewiltshire27@outlook.com>
2024-08-12 20:50:20 +0300

    [REFactor]: Switch from MongoDB to SQLAlchemy
    - Removed MongoDB-related code and imports.
    - Integrated SQLAlchemy for database interactions.
    - Configured SQLAlchemy engine and session.
    - Added `init_db` function to create database tables.
    - Updated to use `Yml` for configuration management.

--- src/module/database.py
+++ src/module/database.py
@@ -1,44 +1,16 @@
-from pymongo import MongoClient
-from pymongo.collection import Collection
-from typing import Any, Dict, List, Optional
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .models import Base
+from .yml import Yml
+
+config = Yml('./config/config.yml')
+data = config.read()
+DATABASE_URL = data.get('Storage', {}).get("URL", {})
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-class PyMongoClient:
-    def __init__(self, uri: str, database_name: str):
-        self.client = MongoClient(uri)
-        self.db = self.client[database_name]
-
-    def get_collection(self, collection_name: str) -> Collection:
-        return self.db[collection_name]
-
-    def insert_one(self, collection_name: str, document: Dict[str, Any]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.insert_one(document)
-
-    def insert_many(self, collection_name: str, documents: List[Dict[str, Any]]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.insert_many(documents)
-
-    def find_one(self, collection_name: str, query: Dict[str, Any]) -> Optional[Dict[str, Any]]:
-        collection = self.get_collection(collection_name)
-        return collection.find_one(query)
-
-    def find_many(self, collection_name: str, query: Dict[str, Any]) -> List[Dict[str, Any]]:
-        collection = self.get_collection(collection_name)
-        return list(collection.find(query))
-
-    def update_one(self, collection_name: str, query: Dict[str, Any], update: Dict[str, Any]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.update_one(query, {'$set': update})
-
-    def update_many(self, collection_name: str, query: Dict[str, Any], update: Dict[str, Any]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.update_many(query, {'$set': update})
-
-    def delete_one(self, collection_name: str, query: Dict[str, Any]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.delete_one(query)
-
-    def delete_many(self, collection_name: str, query: Dict[str, Any]) -> None:
-        collection = self.get_collection(collection_name)
-        collection.delete_many(query)
+def init_db():
+    Base.metadata.create_all(bind=engine)
