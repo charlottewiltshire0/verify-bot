fc7b24e8f1e46d7c30aa2380f7853f84f2c5d90b
StarStudio <charlottewiltshire27@outlook.com>
2024-08-16 18:03:55 +0300

    [FEATURE] Add guilds to database on bot start
    - Added `add_guilds_to_db` method to register guilds in the database when the bot starts.
    - Updated `on_ready` event to call `add_guilds_to_db` to ensure all guilds are added to the database.
    - Integrated `scoped_session` for managing SQLAlchemy sessions in the `OnReady` cog.
    - Added error handling and logging for database operations during guild addition.

--- src/events/ready.py
+++ src/events/ready.py
@@ -3,8 +3,9 @@ import asyncio
 import disnake
 from disnake.ext import commands
 from loguru import logger
+from sqlalchemy.orm import scoped_session
 
-from src.module import Yml, TextFormatter
+from src.module import Yml, TextFormatter, SessionLocal, Verify
 
 
 class OnReady(commands.Cog):
@@ -12,10 +13,12 @@ class OnReady(commands.Cog):
         self.bot = bot
         self.activity_settings = Yml("./config/config.yml").load().get("BotActivitySettings", {})
         self.formatter = TextFormatter(bot)
+        self.session = scoped_session(SessionLocal)
 
     @commands.Cog.listener()
     async def on_ready(self):
         logger.info(f"{self.bot.user} is now online.")
+        await self.add_guilds_to_db()
         await self.update_activity()
 
     async def update_activity(self):
@@ -44,6 +47,24 @@ class OnReady(commands.Cog):
                 activity = disnake.Activity(type=activity_type, name=formatted_status)
                 await self.bot.change_presence(activity=activity)
                 await asyncio.sleep(interval)
+
+    async def add_guilds_to_db(self):
+        session = self.session()
+        try:
+            for guild in self.bot.guilds:
+                existing_entry = session.query(Verify).filter_by(guild=guild.id).first()
+                if not existing_entry:
+                    new_entry = Verify(
+                        guild=guild.id
+                    )
+                    session.add(new_entry)
+            session.commit()
+            logger.info("Guilds have been added to the database.")
+        except Exception as e:
+            logger.error(f"Error occurred while adding guilds to the database: {e}")
+            session.rollback()
+        finally:
+            session.close()
 
 
 def setup(bot: commands.Bot):
