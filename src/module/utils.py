ce2ee86fe1c4bfcc4d2b34aa37618038905ee7de
StarStudio <charlottewiltshire27@outlook.com>
2024-08-18 22:46:10 +0300

    [ADD] New Utility Function for Sending Embeds to Members in `utils.py`
    - Added `send_embed_to_member` function to streamline sending embeds to members with error handling.
    - Updated logging function to print errors if the logging channel is not found.
    
    **Files Modified:**
    - `src/module/utils.py`

--- src/module/utils.py
+++ src/module/utils.py
@@ -431,3 +431,11 @@ async def log_action(bot: commands.Bot, logging_channel_id: int, embed_factory, 
         await channel.send(embed=embed)
     else:
         print(f"Logging channel with ID {logging_channel_id} not found.")
+
+
+async def send_embed_to_member(self, member, preset, color_type):
+    embed = self.embed_factory.create_embed(preset=preset, color_type=color_type)
+    try:
+        await member.send(embed=embed)
+    except Exception as e:
+        logger.error(f"Failed to send DM to {member}: {e}")
