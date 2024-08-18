050709c8c9cd065e65aca8c52a4f3d9848bebfd0
StarStudio <charlottewiltshire27@outlook.com>
2024-08-18 16:59:00 +0300

    [UPDATE] Enhance Verification Commands with Logging and Error Handling
    - Integrated logging functionality for verification actions:
      - Success: Logs verification success events to a specified channel.
      - Rejection: Logs verification rejection events to a specified channel.
      - Removed Verification: Logs user removal from verification.
    - Added `log_action` method to handle logging of different verification actions.
    - Improved error handling and feedback:
      - Added log warnings for missing logging channels.
    - Updated embed responses to include success color when appropriate.
    
    **Files Updated:**
    - `src/commands/moderator/verify.py`

--- src/commands/moderator/verify.py
+++ src/commands/moderator/verify.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
-from disnake.ext.commands import has_permissions, MissingPermissions
+from disnake.ext.commands import MissingPermissions
+from loguru import logger
 
 from src.buttons.verifyButton import VerifyButton
 from src.module import EmbedFactory, VerifyUtils, Yml
@@ -13,6 +14,21 @@ class Verify(commands.Cog):
         self.verify_utils = VerifyUtils()
         self.verify_settings = Yml("./config/config.yml").load().get("Verify", {})
         self.staff_roles = [int(role_id) for role_id in self.verify_settings.get("StaffRoles", [])]
+
+        self.logging_enabled = Yml("./config/config.yml").load().get('Logging', {}).get('Verify', {}).get('Enabled', False)
+        self.logging_channel_id = int(Yml("./config/config.yml").load().get('Logging', {}).get('Verify', {})
+                                      .get('ChannelID', 0))
+
+    async def log_action(self, action: str, member: disnake.Member, color: str = None):
+        if not self.logging_enabled or not self.logging_channel_id:
+            return
+
+        channel = self.bot.get_channel(self.logging_channel_id)
+        if channel:
+            embed = await self.embed_factory.create_embed(preset=action, user=member, color_type=color)
+            await channel.send(embed=embed)
+        else:
+            logger.warning(f"Logging channel with ID {self.logging_channel_id} not found.")
 
     async def check_self_verification(self, interaction: disnake.AppCmdInter, member: disnake.Member) -> bool:
         if member.id == interaction.author.id:
@@ -56,14 +72,10 @@ class Verify(commands.Cog):
 
         if view.value is True:
             self.verify_utils.verify_user(member.id, interaction.guild.id, interaction.user.id)
-            embed = await self.embed_factory.create_embed(preset='VerifySuccess', user=member,
-                                                          color_type="Success", ephemeral=True)
-            await interaction.followup.send(embed=embed)
+            await self.log_action('LogVerifySuccess', member, "Success")
         elif view.value is False:
             self.verify_utils.give_rejection(member.id, interaction.guild.id)
-            embed = await self.embed_factory.create_embed(preset='VerifyRejection', user=member,
-                                                          color_type="Error", ephemeral=True)
-            await interaction.followup.send(embed=embed)
+            await self.log_action('LogVerifyRejection', member, "Error")
 
     @verify_slash.sub_command(
         name="remove",
@@ -77,9 +89,9 @@ class Verify(commands.Cog):
             return
 
         self.verify_utils.unverify_user(member.id, interaction.guild.id)
-        embed = await self.embed_factory.create_embed(preset='VerifyRemoved', user=member)
-        await interaction.response.defer()
-        await interaction.followup.send(embed=embed, ephemeral=True)
+        await self.log_action('VerifyRemoved', member)
+        embed = await self.embed_factory.create_embed(preset='VerifyRemoved', user=member, color_type="Success")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @verify_add_slash.error
     @verify_remove_slash.error
