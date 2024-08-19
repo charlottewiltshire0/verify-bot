431f1a543dc23857d989935c314d1f1f57050299
StarStudio <charlottewiltshire27@outlook.com>
2024-08-19 12:03:29 +0300

    [REFactor] Update `ChannelMention` Cog: Refactor DB Access and Implement MentionUtils
    - Replaced direct DB session management with `MentionUtils` for channel mention operations.
    - Updated `ChannelMention` commands to use `MentionUtils` methods for adding, removing, and setting channels.
    - Removed unused `get_db` method.
    - Enhanced logging and error handling for channel operations.
    - Updated embed creation and command responses.
    
    **Files Modified:**
    - `src/commands/owner/mention.py`

--- src/commands/owner/mention.py
+++ src/commands/owner/mention.py
@@ -3,31 +3,27 @@ from disnake import Interaction
 from disnake.ext import commands
 from sqlalchemy.orm import Session
 
-from src.module import EmbedFactory, add_channel_mention, remove_channel_mention, set_channel_mention, SessionLocal
+from src.module import EmbedFactory, SessionLocal, Yml, MentionUtils
 
 
 class ChannelMention(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.embed_factory = EmbedFactory('./config/embeds.yml', './config/config.yml', bot=bot)
+        self.mention_utils = MentionUtils()
 
-    def get_db(self):
-        db = SessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
+        self.logging_enabled = Yml("./config/config.yml").load().get('Logging', {}).get('AutoBan', {}).get('Enabled',
+                                                                                                          False)
+        self.logging_channel_id = int(Yml("./config/config.yml").load().get('Logging', {}).get('AutoBan', {})
+                                      .get('ChannelID', 0))
 
     @commands.slash_command(name="mention", description="Отправляет упоминание новым пользователям, уведомляя их о необходимости пройти верификацию на сервер")
     async def mention_slash(self, interaction: disnake.AppCmdInter):
-        """Slash command for user mentions."""
-        embed = await self.embed_factory.create_embed(preset='MentionHelp')
-        await interaction.response.send_message(embed=embed)
+        pass
 
     @mention_slash.sub_command(name="add", description="Установить канал для упоминания новых пользователей")
     async def mention_slash_add(self, interaction: disnake.AppCmdInter, channel: disnake.TextChannel):
-        db: Session = next(self.get_db())
-        channel_added = add_channel_mention(db, interaction.guild.id, channel.id)
+        channel_added = self.mention_utils.add_channel_mention(interaction.guild.id, channel.id)
         if channel_added:
             embed = await self.embed_factory.create_embed(preset='MentionAdd', channel=channel, color_type="Success")
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -37,8 +33,7 @@ class ChannelMention(commands.Cog):
 
     @mention_slash.sub_command(name="remove", description="Удалить канал для упоминания новых пользователей")
     async def mention_slash_remove(self, interaction: disnake.AppCmdInter):
-        db: Session = next(self.get_db())
-        channel_removed = remove_channel_mention(db, interaction.guild.id)
+        channel_removed = self.mention_utils.remove_channel_mention(interaction.guild.id)
         if channel_removed:
             embed = await self.embed_factory.create_embed(preset='MentionDelete', color_type="Success")
             await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -48,9 +43,11 @@ class ChannelMention(commands.Cog):
 
     @mention_slash.sub_command(name="set", description="Изменить канал для упоминания новых пользователей")
     async def mention_slash_set(self, interaction: disnake.AppCmdInter, channel: disnake.TextChannel):
-        db: Session = next(self.get_db())
-        set_channel_mention(db, interaction.guild.id, channel.id)
-        embed = await self.embed_factory.create_embed(preset='MentionSet', channel=channel)
+        success = self.mention_utils.set_channel_mention(interaction.guild.id, channel.id)
+        if success:
+            embed = await self.embed_factory.create_embed(preset='MentionSet', channel=channel, color_type="Success")
+        else:
+            embed = await self.embed_factory.create_embed(preset='MentionSetFailed', color_type="Error")
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
