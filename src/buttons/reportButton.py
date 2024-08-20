dac19d53ed0e8bc92cab2ac9dc19908806190783
StarStudio <charlottewiltshire27@outlook.com>
2024-08-20 14:28:19 +0300

    [FEATURE]: Integrate EmbedFactory and update report rejection handling
    Changes:
    - Added `EmbedFactory` to `ReportButton` for dynamic embed creation.
    - Updated the `report_reject` method to use `EmbedFactory` for generating embed messages.
    - Implemented message deletion in the `report_reject` method by fetching the stored message ID.
    - Commented out the old channel creation code, as it's no longer in use.

--- src/buttons/reportButton.py
+++ src/buttons/reportButton.py
@@ -2,12 +2,15 @@ from typing import Optional
 
 import disnake
 
-from src.module import ReportUtils, Yml
+from src.module import ReportUtils, Yml, EmbedFactory
 
 
 class ReportButton(disnake.ui.View):
-    def __init__(self, report_utils: ReportUtils, report_id: int):
+    def __init__(self, report_utils: ReportUtils, report_id: int, bot):
         super().__init__(timeout=20.0)
+        self.bot = bot
+        self.embed_factory = EmbedFactory('./config/embeds.yml', './config/config.yml', bot=bot)
+
         self.report_settings = Yml("./config/config.yml").load().get("Report", {})
         self.embed_color = Yml("./config/config.yml").load().get("EmbedColors", {})
         self.report_utils = report_utils
@@ -20,45 +23,62 @@ class ReportButton(disnake.ui.View):
             await interaction.response.send_message("Ошибка принятия репорта.", ephemeral=True)
             return
 
-        category_id = int(self.report_settings.get("Channel", {}).get("Category", 0))
-        category = interaction.guild.get_channel(category_id)
-
-        if category is None:
-            embed = disnake.Embed(
-                title="<:crossmark:1272260131677278209> Ошибка!",
-                color=int(self.embed_color.get("Error", "#ff6161").lstrip("#"), 16),
-                description="Не удалось найти категорию для каналов!"
-            )
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            return
-
-        vc_channel = None
-        if self.report_settings.get("Channel", {}).get("VC", False):
-            vc_channel = await interaction.guild.create_voice_channel(
-                name=f"Репорт-{interaction.user.display_name}",
-                category=category
-            )
-
-        await interaction.guild.create_text_channel(
-            name=f"репорт-{interaction.user.display_name}",
-            category=category,
-            topic=self.report_settings.get("Channel", {}).get(
-                "Topic", ""
-            )
-        )
-
-        embed = disnake.Embed(
-            title="<:tick:1272260155190546584> Успех!",
-            color=int(self.embed_color.get("Success", "#6cff61").lstrip("#"), 16),
-            description=f"Репорт принят и назначен вам. Созданы каналы: {vc_channel.mention if vc_channel else ''} {text_channel.mention}."
-        )
-
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        # category_id = int(self.report_settings.get("Channel", {}).get("Category", 0))
+        # category = interaction.guild.get_channel(category_id)
+        #
+        # if category is None:
+        #     embed = disnake.Embed(
+        #         title="<:crossmark:1272260131677278209> Ошибка!",
+        #         color=int(self.embed_color.get("Error", "#ff6161").lstrip("#"), 16),
+        #         description="Не удалось найти категорию для каналов!"
+        #     )
+        #     await interaction.response.send_message(embed=embed, ephemeral=True)
+        #     return
+        #
+        # vc_channel = None
+        # if self.report_settings.get("Channel", {}).get("VC", False):
+        #     vc_channel = await interaction.guild.create_voice_channel(
+        #         name=f"Репорт-{interaction.user.display_name}",
+        #         category=category
+        #     )
+        #
+        # await interaction.guild.create_text_channel(
+        #     name=f"репорт-{interaction.user.display_name}",
+        #     category=category,
+        #     topic=self.report_settings.get("Channel", {}).get(
+        #         "Topic", ""
+        #     )
+        # )
+        #
+        # embed = disnake.Embed(
+        #     title="<:tick:1272260155190546584> Успех!",
+        #     color=int(self.embed_color.get("Success", "#6cff61").lstrip("#"), 16),
+        #     description=f"Репорт принят и назначен вам. Созданы каналы: {vc_channel.mention if vc_channel else ''} {text_channel.mention}."
+        # )
+        #
+        # await interaction.response.send_message(embed=embed, ephemeral=True)
         self.value = True
         self.stop()
 
     @disnake.ui.button(label="Отклонить", style=disnake.ButtonStyle.red, custom_id="report_reject", emoji="⛔")
     async def report_reject(self, button: disnake.ui.Button, interaction: disnake.CommandInteraction):
-        await interaction.response.send_message("123")
+        success = self.report_utils.close_report(self.report_id, interaction.user.id)
+
+        if success:
+            message_id = self.report_utils.get_message_id(self.report_id)
+
+            if message_id:
+                try:
+                    channel = interaction.channel
+                    message = await channel.fetch_message(message_id)
+                    await message.delete()
+                except disnake.NotFound:
+                    pass
+            embed = await self.embed_factory.create_embed(preset="ReportReject", color_type="Success")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+        else:
+            embed = await self.embed_factory.create_embed(preset="ReportRejectError", color_type="Success")
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+
         self.value = False
         self.stop()
