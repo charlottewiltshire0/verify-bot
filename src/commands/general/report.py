65de4c5c1ed3302c05e5c41b9b9fa8cbf215dab8
StarStudio <charlottewiltshire27@outlook.com>
2024-08-22 17:15:45 +0300

    [COMMENT]: Temporarily Commented Out `report_add_slash` Method
    - The `report_add_slash` method, responsible for adding a user to a report, has been commented out.
    - The commented code includes logic for checking staff roles, report existence, member limits, and success/failure responses.
    - This change is intended for further review or refactoring.

--- src/commands/general/report.py
+++ src/commands/general/report.py
@@ -52,42 +52,49 @@ class Report(commands.Cog):
         modal = ReportModal(self.bot)
         await interaction.response.send_modal(modal=modal)
 
-    @report_slash.sub_command(
-        name="add-user",
-        description="Добавить пользователя в репорт"
-    )
-    async def report_add_slash(self, interaction: disnake.AppCmdInter, report_id, member: disnake.Member):
-        if not await self.check_staff_roles(interaction):
-            return
-
-        if await self.check_self_report(interaction, member):
-            return
-
-        report = self.report_utils.get_report_id(report_id)
-        if not report:
-            embed = await self.embed_factory.create_embed(preset="ReportNotFound", color_type="Error")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            return
-
-        member_ids = self.report_utils.get_member_ids(report_id=report_id)
-        if member_ids and len(member_ids) >= self.limit_per_user:
-            embed = await self.embed_factory.create_embed(preset="ReportAddLimitExceeded", color_type="Error")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            return
-
-        success = self.report_utils.add_member_to_report(member_id=member.id, report_id=report_id)
-        if success:
-            if self.logging_enabled:
-                await log_action(bot=self.bot, logging_channel_id=self.logging_channel_id,
-                                 embed_factory=self.embed_factory,
-                                 action='LogReportAddedSuccess', member=interaction.author, color="Success")
-
-            embed = await self.embed_factory.create_embed(preset="ReportAdded", color_type="Success")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-        else:
-            embed = await self.embed_factory.create_embed(preset="ReportAddAlreadyExists", color_type="Error")
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            logger.info(f"Failed to add user {member} to report #{report_id}.")
+    # @report_slash.sub_command(
+    #     name="add-user",
+    #     description="Добавить пользователя в репорт"
+    # )
+    # async def report_add_slash(self, interaction: disnake.AppCmdInter, report_id: int, member: disnake.Member):
+    #     if not await self.check_staff_roles(interaction):
+    #         return
+    #
+    #     if await self.check_self_report(interaction, member):
+    #         return
+    #
+    #     report = self.report_utils.get_report_by_id_or_victim(report_id=report_id)
+    #     if not report:
+    #         embed = await self.embed_factory.create_embed(preset="ReportNotFound", color_type="Error")
+    #         await interaction.response.send_message(embed=embed, ephemeral=True)
+    #         return
+    #
+    #     member_ids = self.report_utils.get_member_ids(report_id=report_id)
+    #     if member_ids and len(member_ids) >= self.limit_per_user:
+    #         embed = await self.embed_factory.create_embed(preset="ReportAddLimitExceeded", color_type="Error")
+    #         await interaction.response.send_message(embed=embed, ephemeral=True)
+    #         return
+    #
+    #     success = self.report_utils.add_member_to_report(member_id=member.id, report_id=report_id)
+    #     if success:
+    #         # Проверка, был ли участник успешно добавлен
+    #         member_ids = self.report_utils.get_member_ids(report_id=report_id)
+    #         if member.id in member_ids:
+    #             if self.logging_enabled:
+    #                 await log_action(bot=self.bot, logging_channel_id=self.logging_channel_id,
+    #                                  embed_factory=self.embed_factory,
+    #                                  action='LogReportAddedSuccess', member=interaction.author, color="Success")
+    #
+    #             embed = await self.embed_factory.create_embed(preset="ReportAdded", color_type="Success")
+    #             await interaction.response.send_message(embed=embed, ephemeral=True)
+    #         else:
+    #             embed = await self.embed_factory.create_embed(preset="ReportAddAlreadyExists", color_type="Error")
+    #             await interaction.response.send_message(embed=embed, ephemeral=True)
+    #             logger.info(f"Failed to add user {member} to report #{report_id}.")
+    #     else:
+    #         embed = await self.embed_factory.create_embed(preset="ReportAddAlreadyExists", color_type="Error")
+    #         await interaction.response.send_message(embed=embed, ephemeral=True)
+    #         logger.info(f"Failed to add user {member} to report #{report_id}.")
 
 
 def setup(bot: commands.Bot):
